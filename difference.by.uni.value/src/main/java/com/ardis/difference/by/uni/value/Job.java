e2a43121511861831b57e46dcd47a280cae1cdea
mamyanl <2012mamyanlilit2012@gmail.com>
2020-05-06 12:09:21 +0400

    Corrected

--- difference.by.uni.value/src/main/java/com/ardis/difference/by/uni/value/Job.java
+++ difference.by.uni.value/src/main/java/com/ardis/difference/by/uni/value/Job.java
@@ -3,6 +3,7 @@ package com.ardis.difference.by.uni.value;
 import com.mongodb.client.MongoClients;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -14,18 +15,24 @@ import java.util.List;
 @Slf4j
 @Component
 public class Job {
+
     private static long withUni, currentOldDataSize, lostDataCount;
     private static DifferenceConfig differenceConfig = new DifferenceConfig();
+    private static String collection;
 
-    public Job() {
+    public Job(@Value("${mongo.db.new}") String databaseNew,
+               @Value("${mongo.db.old}") String databaseOld,
+               @Value("${mongo.db.diff}") String databaseDiff,
+               @Value("${mongo.col}") String collection) {
+        this.collection = collection;
         var mongoClient = MongoClients.create();
-        var mongoTemplateNew = new MongoTemplate(mongoClient, differenceConfig.getDatabaseNew());
+        var mongoTemplateNew = new MongoTemplate(mongoClient, databaseNew);
         var mongoTemplateResult = new MongoTemplate(
-                mongoClient, differenceConfig.getDatabaseDif());
-        mongoTemplateResult.dropCollection(differenceConfig.getCollection());
+                mongoClient, databaseDiff);
+        mongoTemplateResult.dropCollection(collection);
         new MongoTemplate(
-                mongoClient, differenceConfig.getDatabaseOld())
-                .stream(new Query(), Document.class, differenceConfig.getCollection())
+                mongoClient, databaseOld)
+                .stream(new Query(), Document.class, collection)
                 .forEachRemaining(item -> checkCoincidence(item, mongoTemplateNew, mongoTemplateResult));
 
         log.info("Data With Uni = {}", withUni);
@@ -43,8 +50,8 @@ public class Job {
             item.getList("uni", Document.class)
                     .forEach(i -> oldDataUniValueTemp.add((String) i.get("value")));
             Query queryNew = new Query().addCriteria(Criteria.where("uni.value").in(oldDataUniValueTemp));
-            if (!mongoTemplateNew.stream(queryNew, Document.class, differenceConfig.getCollection()).hasNext()) {
-                mongoTemplateResult.insert(item, differenceConfig.getCollection());
+            if (!mongoTemplateNew.stream(queryNew, Document.class, collection).hasNext()) {
+                mongoTemplateResult.insert(item, collection);
                 lostDataCount++;
             }
         }
